ea75024cf7c7169af467abeb132715fcc2bb8954
jadeepaulse <129868770+jadeam0@users.noreply.github.com>
2024-01-13 13:20:37 +0200

    working on slider

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
-function toggleNavbarLinks() {
-   var navbarLinks = document.querySelectorAll('.navbar a:not(:first-child):not(.toggle-button)');
-    
-   for (var i = 0; i < navbarLinks.length; i++) {
-      if (navbarLinks[i].style.display === 'none') {
-        navbarLinks[i].style.display = 'block';
-      } else {
-        navbarLinks[i].style.display = 'none';
-      }
-   }
-  }
+//index.js
+
+$(document).ready(function() {
+  $('.carousel .slide:first-child').addClass('active');
+ 
+  // Next Slide
+  $('.carousel .next').click(function() {
+     var currentSlide = $('.carousel .slide.active');
+     var nextSlide = currentSlide.next();
+ 
+     if(nextSlide.length === 0) {
+       nextSlide = $('.carousel .slide:first-child');
+     }
+ 
+     currentSlide.removeClass('active');
+     nextSlide.addClass('active');
+  });
+ 
+  // Previous Slide
+  $('.carousel .prev').click(function() {
+     var currentSlide = $('.carousel .slide.active');
+     var prevSlide = currentSlide.prev();
+ 
+     if(prevSlide.length === 0) {
+       prevSlide = $('.carousel .slide:last-child');
+     }
+ 
+     currentSlide.removeClass('active');
+     prevSlide.addClass('active');
+  });
+ });
